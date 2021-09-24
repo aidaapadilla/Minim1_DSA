d68f395fc7cc1d7993d33e0d6ad18fab5667e459
Aidaapadilla <aida.padilla.gonzalez.aiko@gmail.com>
2021-09-24 11:48:55 +0200

    version 1

---
+++ src/Productos.java
@@ -0,0 +1,14 @@
+public class Productos
+{
+    private int ventas; //Variable con cada venta
+    private double precio; //Precio estatico
+
+    private void venta_realizada(int numero) {
+        ventas = ventas + numero;
+    }
+    private double get_precio()
+    {
+
+    }
+}
+
