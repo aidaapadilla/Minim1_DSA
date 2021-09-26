c7d23bf75069adc9ed5bb69b7bb670d8f4cd8a23
Aidaapadilla <aida.padilla.gonzalez.aiko@gmail.com>
2021-09-26 10:40:57 +0200

    Version2

--- src/Productos.java
+++ src/Productos.java
@@ -1,14 +1,55 @@
-public class Productos
+public abstract class Productos {
+
+    public abstract void venta_realizada(int numero);
+
+    public abstract double get_precio() ;
+
+
+}
+abstract class Cafe extends Productos{
+    double precio=1;
+    double ventas;
+
+    @Override
+    public double get_precio() {
+        return this.precio;
+    }
+
+    @Override
+    public void venta_realizada(int numero) {
+        this.ventas=this.ventas+numero;
+    }
+}
+abstract class Redvelvet extends Productos{
+    double precio=3.5; //Tall de redvelvet
+    double ventas;
+    @Override
+    public double get_precio() {
+        return this.precio;
+    }
+
+    @Override
+    public void venta_realizada(int numero) {
+        this.ventas=this.ventas+numero;
+    }
+}
+abstract class Cheesecake extends Productos
 {
-    private int ventas; //Variable con cada venta
-    private double precio; //Precio estatico
-
-    private void venta_realizada(int numero) {
-        ventas = ventas + numero;
+    double precio=3.5;
+    double ventas;
+    @Override
+    public double get_precio() {
+        return this.precio;
     }
-    private double get_precio()
-    {
 
+    @Override
+    public void venta_realizada(int numero) {
+        this.ventas=this.ventas+numero;
     }
+}
+abstract class Vurger extends Productos
+{
+    //oh fuck that's vegan
+
 }
 
